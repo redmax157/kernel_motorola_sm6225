7c5a30e94e7edecb2a16b50753b6f99c7af62f82
redmax157 <alessandroredmax0098@gmail.com>
2025-11-02 13:00:37 -0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+SECONDS=0
+set -e
+
+# Set kernel path
+KERNEL_PATH="out/arch/arm64/boot"
+
+# Set kernel file
+OBJ="${KERNEL_PATH}/Image"
+GZIP="${KERNEL_PATH}/Image.gz"
+CAT="${KERNEL_PATH}/Image.gz-dtb"
+
+# Set kernel name
+DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M)"
+KERNEL_NAME="derivative-${DATE}.zip"
+
+# Clone SukiSU repo
+if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/kylieeXD/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main; fi
+
+# Create anykernel
+rm -rf anykernel
+git clone https://github.com/kylieeXD/AK3-Rosemary.git anykernel
+
+function KERNEL_COMPILE() {
+	if [ "$1" == "install" ]; then
+		# Download required package
+		sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc ccache bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y && sudo apt install llvm -y && sudo apt-get install g++-aarch64-linux-gnu -y && sudo apt install libelf-dev -y && sudo apt install neofetch -y && neofetch
+	fi
+
+	# Set environment variables
+	export USE_CCACHE=1
+	export KBUILD_BUILD_HOST=builder
+	export KBUILD_BUILD_USER=khayloaf
+
+	# Create output directory and do a clean build
+	rm -rf out && mkdir -p out
+
+	# Download clang if not present
+	if [[ ! -d "clang" ]]; then mkdir -p clang
+		wget https://github.com/Impqxr/aosp_clang_ci/releases/download/13289611/clang-13289611-linux-x86.tar.xz -O clang.tar.gz
+		tar -xf clang.tar.gz -C clang && if [ -d clang/clang-* ]; then mv clang/clang-*/* clang; fi && rm -rf clang.tar.gz
+	fi
+
+	# Add clang bin directory to PATH
+	export PATH="${PWD}/clang/bin:$PATH"
+
+	# Make the config
+	make O=out ARCH=arm64 guamp_defconfig
+
+	# Build the kernel with clang and log output
+	make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 2>&1 | tee -a out/compile.log
+}
+
+function KERNEL_RESULT() {
+	# Check if build is successful
+	if [ ! -f "$OBJ" ] || [ ! -f "$GZIP" ] || [ ! -f "$CAT" ]; then
+		exit 1
+	fi
+
+	# Copying image
+	cp "$CAT" "$1/kernels/"
+
+	# Add banner
+	cp banner "$1"
+
+	# Created zip kernel
+	cd "$1" && zip -r9 "$2" *
+
+	# Upload kernel
+	RESPONSE=$(curl -s -F "file=@$2" "https://store1.gofile.io/contents/uploadfile" \
+	|| curl -s -F "file=@$2" "https://store2.gofile.io/contents/uploadfile")
+	DOWNLOAD_LINK=$(echo "$RESPONSE" | grep -oP '"downloadPage":"\K[^"]+')
+	echo -e "\nDownload link: $DOWNLOAD_LINK"
+
+	# Back to kernel root
+	cd -
+}
+
+# Run functions for R variant
+KERNEL_COMPILE "$1"
+KERNEL_RESULT anykernel "$KERNEL_NAME"
+
+# Done bang
+echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !\n"
